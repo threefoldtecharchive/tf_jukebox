17bd81d2b688ef5a1cb0bef68122953c298c94f4
ranatrk <rana.t.hassan@gmail.com>
2021-06-06 17:45:27 +0200

    Add ubuntu sample chatflow

---
+++ jumpscale/packages/jukebox/chats/ubuntu.py
@@ -0,0 +1,33 @@
+from jumpscale.loader import j
+from jumpscale.sals.chatflows.chatflows import chatflow_step
+from jumpscale.packages.jukebox.sals.jukebox_deploy_chatflow import JukeboxDeployChatflow
+
+
+class UbuntuDeploy(JukeboxDeployChatflow):
+    # HUB_URL = "https://hub.grid.tf/tf-bootable"
+    # IMAGES = ["ubuntu-18.04", "ubuntu-20.04"]
+    title = "Ubuntu"
+    SOLUTION_TYPE = "ubuntu"
+    FLIST = f"https://hub.grid.tf/tf-bootable/3bot-ubuntu-20.04.flist"
+    steps = [
+        "block_chain_info",
+        "choose_farm",
+        "set_expiration",
+        "upload_public_key",
+        "environment",
+        "payment",
+        "deploy",
+        "success",
+    ]
+    QUERY = {"cru": 1, "sru": 1, "mru": 1}
+
+    @chatflow_step(title="User configurations")
+    def environment(self):
+        self.env = {"pub_key": self.public_key}
+        self.metadata = {
+            "name": "test_node",
+            "form_info": {"chatflow": self.SOLUTION_TYPE, "Solution name": "test_node"},
+        }
+
+
+chat = UbuntuDeploy
